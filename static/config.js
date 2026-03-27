325d7ebb4ac6c7f33a3c56449d7fda8043522332
Ragul12831 <ragul.rajkumar@tessolve.com>
2026-03-27 12:50:25 +0530

    New templates added

--- static/config.js
+++ static/config.js
@@ -1,5 +1,7 @@
 const SERVER_CONFIG = {
-    ip: window.location.hostname,
+    // ip: window.location.hostname,
+    ip: '100.112.10.66',
+    rpi_ip: '100.109.50.57',
     usb_ip: '100.112.10.66',
     pythonsv_url: `https://${window.location.hostname}:4200/`,
     bmc_: `https://${window.location.hostname}:4200/`,
